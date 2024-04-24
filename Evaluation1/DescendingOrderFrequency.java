24847d00a2b43b4555e4f8dffd77bfcaa3262f77
DINESH-2611 <159759453+DINESH-2611@users.noreply.github.com>
2024-04-24 17:47:45 +0530

    Update DescendingOrderFrequency.java

--- Evaluation1/DescendingOrderFrequency.java
+++ Evaluation1/DescendingOrderFrequency.java
@@ -17,7 +17,7 @@ public class DescendingOrderFrequency {
     }
 
     private void frequencySort(int[] arr) {
-        Map<Integer,Integer> map = new HashMap<>();
+        Map<Integer,Integer> map = new LinkedHashMap<>();
         for (int i = 0; i < arr.length; i++) {
             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
